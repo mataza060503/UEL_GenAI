e02a113d93fcc342d780cf87c313b93e8827f83d
vohoanglam <vohoanglam060503@gmail.com>
2025-02-12 11:15:46 +0700

    intallize firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { PagesModule } from './pages/pages.module';
 import { ComponentsModule } from './components/components.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 /** PrimeNg components */
 
@@ -21,7 +24,11 @@ import { ComponentsModule } from './components/components.module';
     PagesModule,
     ComponentsModule,
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"gradutation-project-b8acb","appId":"1:1079223125431:web:9122e22b93878cecd45432","storageBucket":"gradutation-project-b8acb.firebasestorage.app","apiKey":"AIzaSyD4qxusAHbt5jL7I23pjrPoDvJVMnsX6LA","authDomain":"gradutation-project-b8acb.firebaseapp.com","messagingSenderId":"1079223125431","measurementId":"G-32K7QTH7L9"})),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCzv6SoyhaZX0WngbOTJuXm223Kwo305EE",
+    authDomain: "mrs-thanh-freelance-p.firebaseapp.com",
+    databaseURL: "https://mrs-thanh-freelance-p-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mrs-thanh-freelance-p",
+    storageBucket: "mrs-thanh-freelance-p.firebasestorage.app",
+    messagingSenderId: "248531377046",
+    appId: "1:248531377046:web:2ff65ad8147eda61265bb7",
+    measurementId: "G-DP2SE2Z5P6"
+  }
+};
